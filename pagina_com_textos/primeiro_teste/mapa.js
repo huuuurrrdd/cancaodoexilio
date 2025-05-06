5387f6d1f6722e95667029151b713e056cc724da
Ema <emalopesdois@gmail.com>
2025-05-06 15:17:30 +0100

    tentativas de usar leaflet

---
+++ pagina_com_textos/primeiro_teste/mapa.js
@@ -0,0 +1,13 @@
+//https://leafletjs.com/examples/quick-start/
+//https://www.sitepoint.com/leaflet-create-map-beginner-guide/
+
+/*
+let texto_conteiner = document.createElement("div")
+document.querySelector("body").appendChild(texto_conteiner)
+texto_conteiner.className += "texto-container"
+texto_conteiner.innerHTML = "Btata"
+*/
+
+var map = L.map('mapa').setView([51.505, -0.09], 13)
+
+
