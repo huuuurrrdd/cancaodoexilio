3a976777076b5ecc59922736618bf20da88d1460
Ema <emalopesdois@gmail.com>
2025-05-12 13:04:00 +0100

    alterações em ficheiro json (coordenadas)

--- .vs/Tentativa_de_leaflet/js/t2map.js
+++ .vs/Tentativa_de_leaflet/js/t2map.js
@@ -1,14 +1,14 @@
 
 
-const map = L.map('map', {
-    center: [-29.50, 145],
-    zoom: 3.5
-  });
+// const map = L.map('map', {
+//     center: [-29.50, 145],
+//     zoom: 3.5
+//   });
   
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
   
-  // pequeno teste com dados do ficheiro
-  const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
+//   // pequeno teste com dados do ficheiro
+//   const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
 
 
 
@@ -17,7 +17,7 @@ function fetchData(){
 
   let textData
 
-  fetch("./json/pagina_com_textos/primeiro_teste/textos_coordenadas_geograficas.json")
+  fetch("./json/textos_coordenadas_geograficas.json")
     .then(response => {
       if(!response.ok){ // mensagem de erro
         throw new Error(`HTTP error! Status ${response.status}`)
@@ -50,12 +50,28 @@ const map = L.map('map', {
   // pequeno teste com dados do ficheiro
   const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
 
+  //teste para id = 19-1
+
+  const marker = []
+
+  
+
+  // for (i=0; i< textData[19-1].coordenadas_geograficas.length; i++){
+  //   //marker[i] = L.marker(str(textData[19-1].coordenadas_geograficas[i]).replace).addTo(map);
+  //   //console.log(textData[21-1].coordenadas_geograficas)
+  //   console.log(str(textData[19-1].coordenadas_geograficas[i]).replace("(", "["))
+
+  // }
+
     //   // /*********** Display coordenada **************/
     // let cord_container = document.createElement("div")
     // document.querySelector("body").appendChild(cord_container)
     // cord_container.className += "cord-container"
     // cord_container.innerHTML = `<p>${textData[20].id}</p> <p></p>`
-    console.log( `O id: ${textData[20].id}, coordenadas:  ${textData[20].coordenadas_geograficas}`)
+
+
+    //funcionaa!!
+    //console.log( `O id: ${textData[20].id}, coordenadas:  ${textData[5].coordenadas_geograficas[1]}`)
 
 }
 
