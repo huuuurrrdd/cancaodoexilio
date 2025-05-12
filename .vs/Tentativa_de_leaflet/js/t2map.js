c762091c4ee7b21399a7821f1a3bfb1f336b7d71
Ema <emalopesdois@gmail.com>
2025-05-12 14:58:36 +0100

    Primeiras marcas no mapa

--- .vs/Tentativa_de_leaflet/js/t2map.js
+++ .vs/Tentativa_de_leaflet/js/t2map.js
@@ -38,6 +38,10 @@ fetchData()
 
 function displayData(textData){
 
+  dado1 = textData[4-1].coordenadas_geograficas[1].replace("(", "").replace(")","")
+  console.log(`Dado1:${dado1}`)
+  console.log(`Teste latitude:${get_latitude(textData[19-1].coordenadas_geograficas[0])}`)
+  console.log(`Teste longitude:${get_longitude(textData[19-1].coordenadas_geograficas[0])}`)
 
 
 const map = L.map('map', {
@@ -48,37 +52,41 @@ const map = L.map('map', {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
   
   // pequeno teste com dados do ficheiro
-  const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
-  const marker2 = L.marker([39.6621648, -8.1353519]).addTo(map); // com coordenada "normal" funciona!!
-  const marker3 = L.marker(textData[4-1].coordenadas_geograficas[1].replace("(", "[").replace(")","]")).addTo(map); //n funciona!!
+  //const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
+  //const marker2 = L.marker([39.6621648, -8.1353519]).addTo(map); // com coordenada "normal" funciona!!
+  const marker3 = L.marker([get_latitude(textData[19-1].coordenadas_geograficas[0]), get_longitude(textData[19-1].coordenadas_geograficas[0])]).addTo(map); //FUNCIONOUUUU!!!!
 
   //teste para id = 19-1
 
   const marker = []
   const marca = []
 
+  //pode não estar a funciona, porque precisa primeiro do mapa e depois da info ou pq precisa da info antes de criar o mapa!! Pode ser necessário criar um script de fetch antes??!!
+
   console.log(textData[19-1].coordenadas_geograficas[0]) // funcionaa!!
   console.log(textData[19-1].coordenadas_geograficas[0].replace("(", "[").replace(")","]")) // funcionaaa!!!
 
-  // for (i=0; i< textData[19-1].coordenadas_geograficas.length; i++){
-  //   //n funcional:
-  //   //marca[i] = textData[19-1].coordenadas_geograficas[i].replace("(", "[").replace(")","]")
-  //   //marker[i] = L.marker(marca[i]).addTo(map);
+  for (i=0; i< textData[19-1].coordenadas_geograficas.length; i++){
 
-  //   //console.log(textData[21-1].coordenadas_geograficas)
-  //   //console.log(textData[19-1].coordenadas_geograficas[i].replace("(", "[").replace(")","]"))// funcionall
-
-  // }
-
-    //   // /*********** Display coordenada **************/
-    // let cord_container = document.createElement("div")
-    // document.querySelector("body").appendChild(cord_container)
-    // cord_container.className += "cord-container"
-    // cord_container.innerHTML = `<p>${textData[20].id}</p> <p></p>`
+    marker[i] = L.marker([get_latitude(textData[19-1].coordenadas_geograficas[i]), get_longitude(textData[19-1].coordenadas_geograficas[i])]).addTo(map);
 
 
-    //funcionaa!!
-    //console.log( `O id: ${textData[20].id}, coordenadas:  ${textData[5].coordenadas_geograficas[1]}`)
+  }
+
+
+    function get_latitude(element){
+      elemento_limpo = element.replace("(", "").replace(")","")
+      latitude = elemento_limpo.split(", ")[0]
+      return latitude
+    }
+
+    function get_longitude(element){
+      elemento_limpo = element.replace("(", "").replace(")","")
+      longitude = elemento_limpo.split(", ")[1]
+      return longitude
+    }
+
+    
 
 }
 
