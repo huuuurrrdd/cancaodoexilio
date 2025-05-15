653f3c2d4c920241b6baead4fc80404b06584684
Ema <emalopesdois@gmail.com>
2025-05-15 10:20:27 +0100

    sobre o input...ainda

--- pagina_com_textos/primeiro_teste/js/index.js
+++ pagina_com_textos/primeiro_teste/js/index.js
@@ -31,6 +31,9 @@ console.clear()
     document.querySelector("body").appendChild(titulo_container)
     titulo_container.className += "titulo-container"
 
+/*******************************************************/
+
+
 //div para formulário e botões
     let btform_ct = document.createElement("div")
     document.querySelector("body").appendChild(btform_ct)
@@ -174,10 +177,12 @@ function displayData(wordData, textData){
     })
 
 
-    //regressa ao estado inicial
+/////regressa ao estado inicial
     bt_clear.addEventListener("click", () =>{
         clearList(results_pal)
         clearList(filtro_pal_ct)
+        input_pal.value = ""
+        showResults(data) ///n sei esteee!!!
     })
 
 
@@ -195,7 +200,7 @@ function displayData(wordData, textData){
                 //ver como está organizado o json dos gatos!!!
                 resultItem.className += "result-item" + palavra.palavra // certamente que esta mal!! eu preciso associar palavra a id!!
 
-                const text = document.createElement(palavra.palavra)
+                const text = document.createElement(palavra.title) // um primeiro teste com títulos de textos!!
                 resultItem.appendChild(text);
                 results_pal.appendChild(resultItem)
             }
@@ -217,7 +222,192 @@ function displayData(wordData, textData){
 
     /***************** Ordenação dos dados ***************************************/
 
-    //let filter_ct = document.createElement("div")
+    let filter_ct = document.createElement("div")
+    document.querySelector(".btform-ct").appendChild(filter_ct)
+    filter_ct.className = "filter-ct"
+
+    //BT PARA ORDEM ALFABETICA
+    let bt_a_z = document.createElement("button")
+    document.querySelector(".btform-ct").appendChild(bt_a_z)
+    bt_a_z.className += "bt-AZ"
+    bt_a_z.innerHTML = "Alphabetic order: AZ"
+    bt_a_z.addEventListener("click", sortAZ) // acao após click
+
+    let bt_z_a = document.createElement("button")
+    document.querySelector(".btform-ct").appendChild(bt_z_a)
+    bt_z_a.className += "bt-ZA"
+    bt_z_a.innerHTML = "Alphabetic order: ZA"
+    bt_z_a.addEventListener("click", sortZA) // acao após click
+
+
+    //BT PARA ORDEM DE DATAS
+    let bt_date = document.createElement("button")
+    bt_date.innerHTML = "Order by age: Ascend"
+    bt_date.className += "bt-num"
+    document.querySelector(".filter-ct").appendChild(bt_date)
+    bt_date.addEventListener("click", ordNumber) // acao após click
+
+    let bt_date_rev = document.createElement("button")
+    bt_date_rev.innerHTML = "Order by age Descend"
+    bt_date_rev.className += "bt-num-rev"
+    document.querySelector(".filter-ct").appendChild(bt_date_rev)
+    bt_date_rev.addEventListener("click", ordNumberRev) // acao após click
+
+
+    //ACAO ORDEM ALPHABETICA
+    function sortAZ() {
+        clearAll() // n sei se isto ja esta
+        let word = []
+        word.length = textData.length // acho que consigo testar logo com as palavras
+
+
+        for(let i = 0; i < word.length; i++){
+            word[i] = textData[i].title
+        }
+
+
+        word.sort()
+        console.log(sortByReference(word, textData, "word"))
+        showResults(sortByReference(word, textData, "word"))
+    }
+
+
+    //ACAO ORDEM ALPHABETICA (invertida)
+    function sortZA() {
+        clearAll() // n sei se isto ja esta
+        let word = []
+        word.length = textData.length // acho que consigo testar logo com as palavras
+
+        for(let i = 0; i < word.length; i++){
+            word[i] = textData[i].title
+        }
+
+        word.sort()
+        
+
+
+        //inverse word order
+        let reverseWord = []
+        for(let i = textData.length-1; i>= 0; i--) {
+            reverseWord.push(word[i])
+        }
+
+
+        console.log(reverseWord)
+
+        //convert to object and display
+        console.log(sortByReference(reverseWord, textData, "word"))
+        showResults(sortByReference(reverseWord, textData, "word"))
+    }
+
+
+
+    //ACAO ORDEM DATA
+    function ordNumber(){
+        clearAll()
+        let object = textDatadata
+
+        //sort by date
+        object.sort((a, b) => a.date_of_publication - b.date_of_publication)
+
+        console.log(object)
+        showResults(object)
+    }
+
+
+    //ordenar pela idade descendente
+    function ordNumberRev(){
+        clearAll()
+        let object = textData
+
+        //sort by date
+        object.sort((a, b) => a.date_of_publication - b.date_of_publication)
+
+
+        let word = []
+        word.length = textData.length
+
+        //copy data from the object
+        for(let i = 0; i < word.length; i++){
+            word[i] = object[i].title
+        }
+
+        //reverse the word array
+        let reverseWord = []
+        for(let i = word.length - 1; i >= 0; i--){
+            reverseWord.push(word[i])
+        }
+        console.log(reverseWord)
+
+
+        //map the word array with an object
+        console.log(sortByReference(reverseWord, textData, "word"))
+
+        //show the results
+        showResults(sortByReference(reverseWord, textData, "word"))
+        console.log(object)
+
+    }
+
+
+
+    //*********** Atualiza o objeto com base num array *************/
+    const sortByReference = (name, data, type) => {
+            let words = []
+            words.length = textData.length
+
+        const sorted = name.map((element) => {
+            for(let i = 0; i < data.length; i++){
+                
+                if(type == "word"){
+
+                    if(data[i].title === element){
+                        return data[i]
+                    }
+
+                } else if(type == "date"){
+                    if(data[i].date_of_publication === element){
+                        return data[i]
+                    }
+                }
+
+            }
+        })
+
+        return sorted
+
+    }
+
+
+    //*********** Coloca objetos em elementos html *************/
+
+function showResults(object){
+
+    object.forEach((elem) => {
+
+        let elem_container = document.createElement("div")
+        document.querySelector(".filtro-pal-ct").appendChild(elem_container)
+        elem_container.className += "elem-ct elem-ct-" + elem.title
+        elem_container.className += elem.id
+
+        let title_ct = document.createElement("div")
+        document.querySelector(".elem-ct" + elem.title).appendChild(title_ct)
+        title_ct.className += "title-ct-" + elem.title
+
+        let title = document.createElement("h1")
+        document.querySelector(".title-ct-" + elem.title).appendChild(title)
+        title.className += "title"
+
+        let date = document.createElement("div")
+        document.querySelector(".elem-ct-" + elem.title).appendChild(date)
+        //date.
+
+    })
+}
+
+    
+
+
 
 
 
