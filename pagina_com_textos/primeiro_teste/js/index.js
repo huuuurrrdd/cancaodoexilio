45ccbe04a54ca6d6f7155865cbe09042177b07f0
Ema <emalopesdois@gmail.com>
2025-05-15 13:32:58 +0100

    Página pesquisa de palavras- nada está funcional

--- pagina_com_textos/primeiro_teste/js/index.js
+++ pagina_com_textos/primeiro_teste/js/index.js
@@ -1,3 +1,7 @@
+//preciso de fazer uma nova versao desta pagina de pesquisa!!!
+//testando passo a passo...
+
+
 
 /*
     Esta página deve incluir:
@@ -49,6 +53,8 @@ console.clear()
     document.querySelector("body").appendChild(filtro_pal_ct)
     filtro_pal_ct.className += "filtro-pal-ct"
 
+
+/*******************************************************/
 //div para texto explicativo
     let explic_ct = document.createElement("div")
     document.querySelector("body").appendChild(explic_ct)
@@ -104,7 +110,6 @@ function displayData(wordData, textData){
     titulo.innerHTML = "Canção do Exílio"
 
     // /*********** Formulario de pesquisa **************/
-
     //form container
     let form_ct = document.createElement("div")
     document.querySelector(".btform-ct").appendChild(form_ct)
@@ -114,6 +119,11 @@ function displayData(wordData, textData){
     let form = document.createElement("form")
     document.querySelector(".form-ct").appendChild(form)
     form.className += "form"
+    //n sei bem o que isto faz/previne - qual é o default
+    form.addEventListener("submit", e => {
+        e.preventDefault()
+    })
+
 
     //label para inut box (pesquisa)
     let label_pesquisa_pal = document.createElement("label")
@@ -152,8 +162,8 @@ function displayData(wordData, textData){
 
             value = value.trim().toLowerCase()
             
-            const filteredData = data.filter(wordData => wordData.palavra.toLowerCase().includes(value))
-            StyleSheetList(filteredData)
+            const filteredData = wordData.filter(wordData => wordData.palavra.toLowerCase().includes(value))
+            setList(filteredData)
             
         }else{
             clearList(results_pal)
@@ -182,7 +192,7 @@ function displayData(wordData, textData){
         clearList(results_pal)
         clearList(filtro_pal_ct)
         input_pal.value = ""
-        showResults(data) ///n sei esteee!!!
+        showResults(wordData) ///n sei esteee!!!
     })
 
 
@@ -197,13 +207,16 @@ function displayData(wordData, textData){
         } else{
             for(const palavra of results) {
                 const resultItem = document.createElement("li")
-                //ver como está organizado o json dos gatos!!!
-                resultItem.className += "result-item" + palavra.palavra // certamente que esta mal!! eu preciso associar palavra a id!!
+                resultItem.className += "result-item" + palavra.palavra.replace(/\s+/g, "-").toLowerCase()
+                
+   // Quando o utilizador clica numa sugestão
+            resultItem.addEventListener("click", () => {
+                input_pal.value = palavra.palavra;
+                clearList(results_pal);
+                showResults([palavra]);
+            });
 
-                const text = document.createElement(palavra.title) // um primeiro teste com títulos de textos!!
-                resultItem.appendChild(text);
-                results_pal.appendChild(resultItem)
-            }
+            results_pal.appendChild(resultItem);
             showResults(results) // falta a funcaooo
 
         }
@@ -217,7 +230,7 @@ function displayData(wordData, textData){
     }
 
 
-    showResults(data)
+    showResults(wordData)
 
 
     /***************** Ordenação dos dados ***************************************/
@@ -241,34 +254,34 @@ function displayData(wordData, textData){
 
 
     //BT PARA ORDEM DE DATAS
-    let bt_date = document.createElement("button")
-    bt_date.innerHTML = "Order by age: Ascend"
-    bt_date.className += "bt-num"
-    document.querySelector(".filter-ct").appendChild(bt_date)
-    bt_date.addEventListener("click", ordNumber) // acao após click
+    let bt_freq = document.createElement("button")
+    bt_freq.innerHTML = "Order by age: Ascend"
+    bt_freq.className += "bt-num"
+    document.querySelector(".filter-ct").appendChild(bt_freq)
+    bt_freq.addEventListener("click", ordNumber) // acao após click
 
-    let bt_date_rev = document.createElement("button")
-    bt_date_rev.innerHTML = "Order by age Descend"
-    bt_date_rev.className += "bt-num-rev"
-    document.querySelector(".filter-ct").appendChild(bt_date_rev)
-    bt_date_rev.addEventListener("click", ordNumberRev) // acao após click
+    let bt_freq_rev = document.createElement("button")
+    bt_freq_rev.innerHTML = "Order by age Descend"
+    bt_freq_rev.className += "bt-num-rev"
+    document.querySelector(".filter-ct").appendChild(bt_freq_rev)
+    bt_freq_rev.addEventListener("click", ordNumberRev) // acao após click
 
 
     //ACAO ORDEM ALPHABETICA
     function sortAZ() {
         clearAll() // n sei se isto ja esta
         let word = []
-        word.length = textData.length // acho que consigo testar logo com as palavras
+        word.length = wordData.length // acho que consigo testar logo com as palavras
 
 
         for(let i = 0; i < word.length; i++){
-            word[i] = textData[i].title
+            word[i] =  wordData[i].palavra
         }
 
 
         word.sort()
-        console.log(sortByReference(word, textData, "word"))
-        showResults(sortByReference(word, textData, "word"))
+        console.log(sortByReference(word,  wordData, "word"))
+        showResults(sortByReference(word,  wordData, "word"))
     }
 
 
@@ -276,10 +289,10 @@ function displayData(wordData, textData){
     function sortZA() {
         clearAll() // n sei se isto ja esta
         let word = []
-        word.length = textData.length // acho que consigo testar logo com as palavras
+        word.length =  wordData.length // acho que consigo testar logo com as palavras
 
         for(let i = 0; i < word.length; i++){
-            word[i] = textData[i].title
+            word[i] =  wordData[i].palavra
         }
 
         word.sort()
@@ -288,7 +301,7 @@ function displayData(wordData, textData){
 
         //inverse word order
         let reverseWord = []
-        for(let i = textData.length-1; i>= 0; i--) {
+        for(let i =  wordData.length-1; i>= 0; i--) {
             reverseWord.push(word[i])
         }
 
@@ -296,8 +309,8 @@ function displayData(wordData, textData){
         console.log(reverseWord)
 
         //convert to object and display
-        console.log(sortByReference(reverseWord, textData, "word"))
-        showResults(sortByReference(reverseWord, textData, "word"))
+        console.log(sortByReference(reverseWord,  wordData, "word"))
+        showResults(sortByReference(reverseWord,  wordData, "word"))
     }
 
 
@@ -305,10 +318,10 @@ function displayData(wordData, textData){
     //ACAO ORDEM DATA
     function ordNumber(){
         clearAll()
-        let object = textDatadata
+        let object =  wordData
 
-        //sort by date
-        object.sort((a, b) => a.date_of_publication - b.date_of_publication)
+        //sort by freq
+        object.sort((a, b) => a.frequency - b.frequency)
 
         console.log(object)
         showResults(object)
@@ -318,18 +331,18 @@ function displayData(wordData, textData){
     //ordenar pela idade descendente
     function ordNumberRev(){
         clearAll()
-        let object = textData
+        let object =  wordData
 
-        //sort by date
-        object.sort((a, b) => a.date_of_publication - b.date_of_publication)
+        //sort by freq
+        object.sort((a, b) => a.frequency - b.frequency)
 
 
         let word = []
-        word.length = textData.length
+        word.length =  wordData.length
 
         //copy data from the object
         for(let i = 0; i < word.length; i++){
-            word[i] = object[i].title
+            word[i] = object[i].word
         }
 
         //reverse the word array
@@ -341,10 +354,10 @@ function displayData(wordData, textData){
 
 
         //map the word array with an object
-        console.log(sortByReference(reverseWord, textData, "word"))
+        console.log(sortByReference(reverseWord,  wordData, "word"))
 
         //show the results
-        showResults(sortByReference(reverseWord, textData, "word"))
+        showResults(sortByReference(reverseWord,  wordData, "word"))
         console.log(object)
 
     }
@@ -354,19 +367,19 @@ function displayData(wordData, textData){
     //*********** Atualiza o objeto com base num array *************/
     const sortByReference = (name, data, type) => {
             let words = []
-            words.length = textData.length
+            words.length =  wordData.length
 
         const sorted = name.map((element) => {
             for(let i = 0; i < data.length; i++){
                 
                 if(type == "word"){
 
-                    if(data[i].title === element){
+                    if(data[i].palavra === element){
                         return data[i]
                     }
 
-                } else if(type == "date"){
-                    if(data[i].date_of_publication === element){
+                } else if(type == "freq"){
+                    if(data[i].frequency === element){
                         return data[i]
                     }
                 }
@@ -381,26 +394,35 @@ function displayData(wordData, textData){
 
     //*********** Coloca objetos em elementos html *************/
 
-function showResults(object){
 
-    object.forEach((elem) => {
+    //estou a corrigir a show results...
+function showResults(words){
+    clearList(filtro_pal_ct)
+
+    words.forEach((word) => {
+
+        let safe_palavra = word.palavra.replace(/\s+/g,"-").toLowerCase()
 
         let elem_container = document.createElement("div")
         document.querySelector(".filtro-pal-ct").appendChild(elem_container)
-        elem_container.className += "elem-ct elem-ct-" + elem.title
-        elem_container.className += elem.id
+        elem_container.className += ` elem-ct elem-ct-${safe_palavra} id-${word.id}`
 
-        let title_ct = document.createElement("div")
-        document.querySelector(".elem-ct" + elem.title).appendChild(title_ct)
-        title_ct.className += "title-ct-" + elem.title
+        let pala_ct = document.createElement("div")
+        document.querySelector(`.elem-ct-${safe_palavra}`).appendChild(pala_ct)
+        pala_ct.className += `pala-ct-${safe_palavra}`
 
-        let title = document.createElement("h1")
-        document.querySelector(".title-ct-" + elem.title).appendChild(title)
-        title.className += "title"
+        let palavra = document.createElement("h1")
+        document.querySelector(`.pala-ct-${safe_palavra}`).appendChild(palavra)
+        palavra.className += "pala"
 
-        let date = document.createElement("div")
-        document.querySelector(".elem-ct-" + elem.title).appendChild(date)
-        //date.
+        let freq = document.createElement("div")
+        document.querySelector(`.elem-ct-${safe_palavra}`).appendChild(freq)
+        freq.className += "name ele-item"
+
+
+
+        palavra.innerText = word.palavra
+        freq.innerHTML = `<span>Frequência:</span> ${word.frequency}`
 
     })
 }
