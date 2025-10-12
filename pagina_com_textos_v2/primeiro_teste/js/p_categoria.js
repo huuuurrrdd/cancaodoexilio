dcae5c9b83d9c03056fa61b2b4f706601306e253
Ema <emalopesdois@gmail.com>
2025-10-12 12:30:15 +0100

    template pág categoria (locais, fauna, flora)

--- pagina_com_textos_v2/primeiro_teste/js/p_categoria.js
+++ pagina_com_textos_v2/primeiro_teste/js/p_categoria.js
@@ -4,6 +4,17 @@
 
 */
 
+//*************  Buscar parametro de categoria  ****************/
+function getQueryParam(param){
+    let urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get(param)
+}
+
+//obter nome de categoria
+let categoria = getQueryParam("categoria")
+
+console.log(categoria) // funciona!!
+
 
 //*************  Acesso a dados  ****************/
 function fetchData(){
@@ -52,8 +63,76 @@ fetchData()
 function displayData(wordData, textData){
 
     let categoria_container = document.createElement("div")
-    document.querySelector(body).appendChild(categoria_container)
+    document.querySelector("body").appendChild(categoria_container)
     categoria_container.className += "categoria-container"
+
+    let subtitulo = document.createElement("p")
+    document.querySelector(".categoria-container").appendChild(subtitulo)
+    subtitulo.className += "subtitulo"
+    subtitulo.innerText = "Categoria de palavras:"
+
+    //titulo (Nome de categoria)
+    let categoria_palavras_h = document.createElement("h1")
+    document.querySelector(".categoria-container").appendChild(categoria_palavras_h)
+    categoria_palavras_h.className += "categoria-palavras-h page-title"
+    categoria_palavras_h.innerText = categoria
+
+    //grafico 
+    let grafico_ct = document.createElement("div")
+    document.querySelector(".categoria-container").appendChild(grafico_ct)
+    grafico_ct.className += "grafico-ct"
+    
+    let canvas = document.createElement("canvas")
+    document.querySelector(".grafico-ct").appendChild(canvas)
+    canvas.className += "grafico-categoria"
+    
+    const GP = document.querySelector(".grafico-categoria")
+
+    //*********** Gráfico inicial **********/
+    // neste gráfico é importante poder comparar o nº de textos total com o nº que mencionam
+    new Chart(GP, {
+        type: "bar",
+        data: {
+            labels: ["A", "B", "C", "D"],
+            datasets:[{
+                label: `${categoria} ao longo do tempo`,
+                data: [10, 20, 40, 10],
+                borderWidth: 1
+            }]
+        },
+        options:{
+            scales:{
+                y:{
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+
+    // Lista de todos os nomes de categorias existentes 
+    // (vai ser necessário colocá-los por ordem de frequencia)
+    let list_all_container = document.createElement("div")
+    document.querySelector(".categoria-container").appendChild(list_all_container)
+    list_all_container.className += "list-all-container"
+
+    let ct_head_list = document.createElement("div")
+    document.querySelector(".list-all-container").appendChild(ct_head_list)
+    ct_head_list.className += "ct-head-list"
+
+    ct_head_list.innerHTML = `  <div class = "palavras header">Palavra</div>
+                                <div class = "texto header">Textos</div>
+                                <div class = "frequencia header">Freq</div>`
+    
+    ct_head_list.style.backgroundColor = "yellow"
+
+
+    let container = document.createElement("div")
+    document.querySelector(".list-all-container").appendChild(container)
+    container.className += "container"
+
+
+    //iteracao para display
+
 
 
 }
