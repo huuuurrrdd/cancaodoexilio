5fcca1adb1b3360d74b4bd97cf2b1b44be681ae5
Ema <emalopesdois@gmail.com>
2025-12-01 12:46:02 +0000

    testes em mapa

---
+++ pagina_com_textos_v2/Tentativa_de_leaflet/js/lista.js
@@ -0,0 +1,12 @@
+  /*****  Circulos  *****/
+
+  // let circle = L.circle([-10.3333333, -53.2], {
+  //   color: "#223F29",
+  //   fillColor: "#223F29",
+  //   fillOpacity: 0.5,
+  //   radious: 500
+  // }).addTo(map)
+
+
+
+  /*****  Poligonos  *****/
--- pagina_com_textos_v2/Tentativa_de_leaflet/js/t2map.js
+++ pagina_com_textos_v2/Tentativa_de_leaflet/js/t2map.js
@@ -11,7 +11,7 @@
 function fetchData() {
   let textData;
 
-  fetch("./json/textos_coordenadas_geograficas.json")
+  fetch("./json/t4_textos_loc_fauna_flora.json")
     .then((response) => {
       if (!response.ok) {
         // mensagem de erro
@@ -33,24 +33,38 @@ fetchData();
 function displayData(textData) {
   console.log(
     `Teste latitude:${get_latitude(
-      textData[19 - 1].coordenadas_geograficas[0]
+      textData[19 - 1].categorias.locais.coordenadas_geograficas[0]
     )}`
   );
   console.log(
     `Teste longitude:${get_longitude(
-      textData[19 - 1].coordenadas_geograficas[0]
+      textData[19 - 1].categorias.locais.coordenadas_geograficas[0]
     )}`
   );
 
   const map = L.map("map", {
-    center: [-29.5, 145],
-    zoom: 3.5,
+    center: [10, -30],
+    zoom: 1.5,
   });
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
+
+
+/*********  Teste de icon no mapa  **********/
+var leafletIcon = L.icon ({
+  iconUrl: './icons/m2.svg',
+  shadowUrl:'./icons/s6.png',
+  iconSize: [25, 35],
+  iconAnchor: [12.5,35],
+  // shadowAnchor:[150/2, 171/2], // icon lateral
+  // shadowSize:[341/2, 312/2]
+  shadowAnchor:[7, 30], // icon svg
+  shadowSize:[40/1.5, 43/1.5]
+})
+
 
   // pequeno teste com dados do ficheiro
   //const marker3 = L.marker([get_latitude(textData[19-1].coordenadas_geograficas[0]), get_longitude(textData[19-1].coordenadas_geograficas[0])]).addTo(map); //FUNCIONOUUUU!!!!
@@ -60,18 +74,89 @@ function displayData(textData) {
   const marker = [];
 
   for (t = 0; t < textData.length; t++) {
-    for (i = 0; i < textData[t].coordenadas_geograficas.length; i++) {
+    for (i = 0; i < textData[t].categorias.locais.coordenadas_geograficas.length; i++) {
       //nota: pode ser necessário verificar se os textos apresentam ou nao coordenada!!
       marker[i] = L.marker([
-        get_latitude(textData[t].coordenadas_geograficas[i]),
-        get_longitude(textData[t].coordenadas_geograficas[i]),
-      ])
+        get_latitude(textData[t].categorias.locais.coordenadas_geograficas[i]),
+        get_longitude(textData[t].categorias.locais.coordenadas_geograficas[i]),
+      ], {icon:leafletIcon})
         .addTo(map)
         .bindPopup(
-          `${textData[t].locais_limpos[i]}, TEXTO: ${textData[t].title}`
+          `${textData[t].categorias.locais.locais_limpos[i]}, TEXTO: ${textData[t].title}`
         );
     }
   }
+
+
+
+const mark = L.marker([9.5250254, -13.6826763], {icon:leafletIcon}).addTo(map)
+
+// Testar 2 no mesmo sitio//
+const arraycircle = []
+
+for(let i = 0; i < 3; i++){
+  arraycircle[i] = L.circle([-10.3333333, (-53.2) +i], {
+    color: "#223F29",
+    fillColor: '#223f29a4',
+    fillOpacity: 1,
+    radius: 50000
+  }).addTo(map)
+  .bindPopup("<b>Alooo</b> <br> <br> MARKER")
+}
+
+//   /*****  Circulos  *****/
+//   let circle = L.circle([-10.3333333, -53.2], {
+//     color: "#223F29",
+//     fillColor: '#223f29a4',
+//     fillOpacity: 1,
+//     radius: 20000
+//   }).addTo(map)
+
+
+
+
+
+//   /*****  Poligonos  *****/
+// let polygon = L.polygon([
+//   [-10.3333333, -53.2],
+//   [28.3347722, -10.3713379],
+//   [9.5250254, -13.6826763]
+// ], {
+//   color: "#223F29",
+//     fillColor: '#223f29a4',
+//     fillOpacity: 1
+// }).addTo(map)
+
+
+
+//   /*****  Poligonos  *****/ // Teste de poligono (caso queira criar área de abrangencia do poema)
+// let polygon = L.polygon([
+//   [39.6621648, -8.135351],
+//   [51.0, 10.],
+//   [39.5428862, -8.919552],
+//   [41.5314496, -8.619230],
+//   [41.5084468, -6.773302],
+//   [33.8439408, 9.40013],
+//   [37.1734995, -3.599533],
+//   [28.3347722, -10.371337],
+//   [34.0346534, -5.016192],
+//   [-10.3333333, -53.],
+//   [9.5250254, -13.682676],
+//   [-22.8169023, -43.127220],
+//   [13.8905104, -2.574941],
+//   [38.569601, -8.901165],
+//   [38.8355446, -9.352237],
+//   [-22.9247351, -43.232716]
+// ], {
+//   color: "#223F29",
+//     fillColor: '#223f29a4',
+//     fillOpacity: 1
+// }).addTo(map)
+
+
+
+
+
 
   function get_latitude(element) {
     elemento_limpo = element.replace("(", "").replace(")", "");
