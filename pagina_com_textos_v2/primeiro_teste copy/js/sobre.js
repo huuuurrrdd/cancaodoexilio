4060eacafc5b19a183f701c397d3f618599d18ed
Ema <emalopesdois@gmail.com>
2026-01-05 14:40:09 +0000

    atualização da página sobre

--- pagina_com_textos_v2/primeiro_teste copy/js/sobre.js
+++ pagina_com_textos_v2/primeiro_teste copy/js/sobre.js
@@ -13,13 +13,13 @@ titulo.className += "titulo page-title"
 
 let conteudo = document.createElement("div")
 margem_ct.appendChild(conteudo)
-conteudo.className = "texto-conteudo"
+conteudo.className = "texto-conteudo conteudo"
 
 titulo.innerHTML = "Sobre"
 
-conteudo.innerHTML = `  Em 1843, na cidade de Coimbra, o estudante brasileiro Antônio Gonçalves Dias compôs o poema <i>Canção do Exílio</i> como uma expressão de nostalgia pela sua pátria de origem. 
+conteudo.innerHTML = `  <p>Em 1843, na cidade de Coimbra, o estudante brasileiro Antônio Gonçalves Dias compôs o poema <i>Canção do Exílio</i> como uma expressão de nostalgia pela sua pátria de origem. 
                         Desde a sua publicação em 1846, a obra tem inspirado inúmeras interpretações na forma de pastiches e paródias. Algumas dessas respostas enaltecem a cultura e a geografia do Brasil, 
-                        enquanto outras criticam e expõem os problemas do país.
+                        enquanto outras criticam e expõem os problemas do país.</p>
                         
                         <p>Esta plataforma visa ser uma ferramenta de exploração do conjunto dos vários textos escritos a partir da Canção do Exílio ao longo dos anos, permitindo observar os textos interrelacionados e transformações ocorridas.</p>
                         <p>O ponto principal de ligação entre os poemas são as palavras que os compõem. É através das palavras que se estabelecem relações entre textos, se descobrem novos percursos de leitura e se revelam recorrências temáticas e simbólicas.</p>
@@ -27,7 +27,29 @@ conteudo.innerHTML = `  Em 1843, na cidade de Coimbra, o estudante brasileiro An
                         <p>A organização da plataforma está assente em categorias, que funcionam como unidades estruturais e analíticas. Considera-se categoria qualquer palavra, tema ou expressão geral que permita agrupar textos ou informações associadas. 
                         Estas categorias possibilitam compreender de que forma determinados elementos surgem, se mantêm ou se transformam ao longo do tempo.</p>
                         <p>As categorias atualmente presentes são: textos, palavras, locais, fauna, flora, anos e autores.</p>
-                        <h2>Outras representações da <i>Canção do Exílio</i></h2>
-                        <p>Este projeto pertence a um estudo desenvolvido por Joshua Enslen, no qual foi construído um corpus de cerca de 500 intertextos variados. 
+                        `
+
+let titulo2 = document.createElement("h2")
+margem_ct.appendChild(titulo2)
+titulo2.className = "titulo2"
+titulo2.innerHTML = `Outras representações da <i>Canção do Exílio</i>`
+
+let conteudo2 = document.createElement("div")
+margem_ct.appendChild(conteudo2)
+conteudo2.className = "texto-conteudo conteudo2"
+
+conteudo2.innerHTML = ` <p>Este projeto pertence a um estudo desenvolvido por Joshua Enslen, no qual foi construído um corpus de cerca de 500 intertextos variados. 
                         A partir desse corpus, foi aplicada uma metodologia que identifica instâncias de palavras e expressões relevantes do poema original em outras obras. 
-                        Baseado desta análise, foram criadas diversas representações da Canção do Exílio:</p>`
+                        Baseado desta análise, foram criadas diversas representações da Canção do Exílio:</p>
+                        
+                        <h4> Livro <i>Song of Exile: A cultural History of Brazil's most popular poem, 1846-2018</i></h4>
+                        <p class = "subtopico">Um primeiro estudo abrangente sobre a influência cultural e literária da <i>Canção do Exílio</i>, desde a sua criação até às reinterpretações contemporâneas, com especial atenção às transformações históricas, políticas e tecnológicas.</p>
+                        <div><img class = "sobre-img sobre-img-liv" src="imagens/livro.jpg"></div>
+
+                        <h4>Exposição <i>Bird-watching: Visualizações da Influência de Canção do Exílio</i></h4>
+                        <p class = "subtopico">Três exposições realizadas por Joshua Enslen e Alaina Enslen e exibidas no <i>Museu da Ciência da Universidade de Coimbra</i>, <i>Festival Literário Internacional de Óbidos</i> e <i>Edifício Caleidoscópio da Universidade de Lisboa</i>.</p>
+                        <div><img class = "sobre-img sobre-img-exp" src = "./imagens/bird.png"></div>
+                        
+                        <h4>Dissertação <i>As Canções do Exílio</i></h4>
+                        <p class = "subtopico">Website com visualizações interativas elaborado no âmbito da dissertação de Mestrado “As Canções do Exílio” de Adriana Barbosa.</p>
+                        <div><img class = "sobre-img sobre-img-dis" src="imagens/bird3.png"></div>`
